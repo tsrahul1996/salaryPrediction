cb939ca842b42228962117dff5fb0449f3b9ec44
tsrahul1996 <tsrahul1996@gmail.com>
2022-05-31 20:04:10 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from joblib import load
+
+app = Flask(__name__)
+model = load('salaryModel.pkl')
+
+
+# GET REQUEST
+@app.route('/')
+def welcome():
+    return "Welcome All"
+
+
+# GET REQUEST
+@app.route('/predict')
+def predict_survival():
+    experience = request.args.get("experience")
+    prediction = model.predict([[int(experience)]])
+    return "the predicted value is" + str(prediction)
+
+if __name__ == '__main__':
+    app.run()
